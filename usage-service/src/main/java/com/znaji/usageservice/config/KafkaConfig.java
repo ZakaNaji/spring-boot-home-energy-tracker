b80bcb6b54497401a7e8a8628a43bb32452ff684
ZakaNaji <zakaria.naji@edu.uca.ma>
2026-04-25 18:05:32 +0100

    config: kafka topics config

---
+++ usage-service/src/main/java/com/znaji/usageservice/config/KafkaConfig.java
@@ -0,0 +1,23 @@
+package com.znaji.usageservice.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KafkaConfig {
+    @Bean
+    public NewTopic energyReadingIngestedTopic(
+            @Value("${app.kafka.topics.energy-reading-ingested}") String topicName
+    ) {
+        return new NewTopic(topicName, 1, (short) 1);
+    }
+
+    @Bean
+    public NewTopic energyAnomalyDetectedTopic(
+            @Value("${app.kafka.topics.energy-anomaly-detected}") String topicName
+    ) {
+        return new NewTopic(topicName, 1, (short) 1);
+    }
+}
